6ac5af070aaf252397627a13ae74f7a1e58cf396
RodrigoRVSN <rodrigovictor81@gmail.com>
2021-10-24 21:35:35 -0300

    🎇 - SSR getUsers

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -24,13 +24,16 @@ import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
-import { useUsers } from "../../services/hooks/useUsers";
+import { getUsers, useUsers } from "../../services/hooks/useUsers";
 import { queryClient } from "../../services/queryClient";
 import { api } from "../../services/api";
+import { GetServerSideProps } from "next";
 
-export default function UserList() {
+export default function UserList({ users }) {
   const [page, setPage] = useState(1);
-  const { data, isLoading, error, isFetching, refetch } = useUsers(page);
+  const { data, isLoading, error, isFetching, refetch } = useUsers(page, {
+    initialData: users,
+  });
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -145,3 +148,8 @@ export default function UserList() {
     </Box>
   );
 }
+
+export const getServiderSideProps: GetServerSideProps = async () => {
+  const { users, totalCount } = await getUsers(1);
+  return { props: { users, totalCount } };
+};
--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useQuery, UseQueryOptions, UseQueryResult } from "react-query";
 import { api } from "../api";
 
 type User = {
@@ -34,8 +34,9 @@ export async function getUsers(page: number): Promise<GetUsersResponse> {
   return { users, totalCount };
 }
 
-export function useUsers(page: number) {
+export function useUsers(page: number, options: UseQueryOptions) {
   return useQuery(["users", page], () => getUsers(page), {
     staleTime: 1000 * 10 * 60,
-  });
+    ...options,
+  }) as UseQueryResult<GetUsersResponse, unknown>;
 }
